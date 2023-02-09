3c6a965028714b96b164e1f4c8613a64f5bff0ee
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-02-09 16:51:21 +0200

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,19 +1,17 @@
 import random
 
 def play():
-    user= input("Whats yur choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
-    computer= random.choice(['r', 'p', 's'])
-
-    if user== computer:
-         return 'tie'
-    if is_win(user, computer):
-        return 'You won!'
-    return "You lost!"
-    
-
-def is_win(player, opponent):
-    if (player== 'r' and opponent=='s') or (player== 's' and opponent=='p') or (player== 'p' and opponent=='r'):
+    user= input("choose 'r' for Rock, 'p' for paper, 's' for scissors\n")
+    computer= random.choice(['r', 's', 'p'])
+    if user== computer: 
+        return 'its  a tie'
+    if player_win(user, computer):
+            return 'yay!! you have won!!'
+    else: 
+            return "Im sorry try again"
+def player_win(player, opponent):
+    if(player=='r' and opponent== 's') or (player=='s' and opponent== 'p') or (player=='p' and opponent== 'p'):
         return True
 
-
 print(play())
+    
