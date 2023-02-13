fd09ed75d764d41c22920926e363982c14a8e89d
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-02-13 15:31:54 +0200

    Update unique_letters.py

--- unique_letters.py
+++ unique_letters.py
@@ -6,7 +6,7 @@ def unique_english_letters(word):
     for letter in letters:
         if letter in word:
             count += 1
-    print(f"there are {count} unique letters")
+    print(f"you have {count} unique letters")
 
 
-print(unique_english_letters(input("enter a word: ")))
+print(unique_english_letters(input("choose a word: ")))
