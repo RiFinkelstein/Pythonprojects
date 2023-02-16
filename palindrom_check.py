0cfa046227b312269c6084c3eef76053088e8346
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-02-16 17:42:43 +0200

    Update palindrom_check.py

--- palindrom_check.py
+++ palindrom_check.py
@@ -1,20 +1,8 @@
 
-# function to check if string is palindrome or not
-def isPalindrome(str):
+word = str(input("enter a word: "))
+backwords = word[::-1]
 
-    # Run loop from 0 to len/2
-    for i in range(0, len(str)//2):
-        if str[i] != str[len(str)-i-1]:
-            return False
-    return True
-
-
-# main function
-s = input("Enter a string: ")
-
-ans = isPalindrome(s)
-
-if (ans):
-    print("Yes")
+if word == backwords:
+    print("your word is a palindrome")
 else:
-    print("No")
+    print("your word is not a palindrom")
