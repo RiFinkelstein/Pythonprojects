17884faaa22d3f93da1bb78ecab8d20bb7af4760
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-02-16 18:04:54 +0200

    Create even_odd_numbers.py

---
+++ even_odd_numbers.py
@@ -0,0 +1,13 @@
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+even_list = []
+odd_list = []
+
+for x in list:
+    if x % 2 == 0:
+        even_list.append(x)
+    if x % 2 != 0:
+        odd_list.append(x)
+print(f"the full list is: {a}")
+print(f"the even numbers are: {even_list}")
+print(f"he odd numbers are: {odd_list}")
