c8769ed9a59f236e3c7e7f942a8582547a2c6b81
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-02-16 19:03:25 +0200

    Update number_guess.py

--- number_guess.py
+++ number_guess.py
@@ -2,11 +2,16 @@ import random
 
 random_number = random.randint(1, 9)
 
-guess = int(input("enter a number: "))
+guess = 0
+count = 0
 
-if guess == random_number:
-    print("yayy!! you won!!")
-if guess > random_number:
-    print("nope, go lower")
-if guess < random_number:
-    print("nope,higher")
+while guess != random_number:
+    guess = int(input("enter a number between 1 and 9: "))
+    count += 1
+    if guess > random_number:
+        print("nope, go lower")
+    elif guess < random_number:
+        print("nope,higher")
+    else:
+        print("correct")
+        print(f"you took {count} tries")
