002f79b36cc237fad40730822ee2e6c37d51d219
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-02-17 15:53:36 +0200

    Create prime_number.py

---
+++ prime_number.py
@@ -0,0 +1,14 @@
+import sys
+number = 12
+prime = False
+if number > 0:
+    for x in range(2, number-1):
+        if number % x != 0:
+            continue
+        elif number % x == 0:
+            sys.exit("your number is not prime")
+        sys.exit("the number is prime")
+if number == 0:
+    sys.exit("your numebr is not prime")
+else:
+    sys.exit("your numebr is not prime")
