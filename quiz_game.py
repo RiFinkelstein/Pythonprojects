7cac45ceb3e4d22d77bb0cb6b5f94e29aaaa62f6
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-02-23 21:32:03 +0200

    Update quiz_game.py

--- quiz_game.py
+++ quiz_game.py
@@ -1,15 +1,32 @@
 print("welcome to my quiz!")
 
-playing= input("do you want to play? ")
-score=0
+playing = input("do you want to play? ")
+score = 0
 if playing != "yes":
     quit()
 
 print("okay! lets play:)")
 
-answer = input("what is my full name? ").lower
-if answer== "rivka finkelstein":
+answer = input("what is my full name? ").lower()
+if answer == "rivka finkelstein":
     print('correct!')
-    score+=1
+    score += 1
 else:
     print("incorrect")
+
+answer = input("What month was I born in ").lower()
+if answer == "july":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect")
+
+answer = input("Where did i grow up? ").lower()
+if answer == "woodmere":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect")
+
+
+print(f"you got {score}/3 correct!!")
