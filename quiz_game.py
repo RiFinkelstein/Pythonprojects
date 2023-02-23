530d7370f7b3e34fc5d34f0f8e9310d8e8e1be48
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-02-23 21:25:21 +0200

    Update quiz_game.py

--- quiz_game.py
+++ quiz_game.py
@@ -1,8 +1,15 @@
 print("welcome to my quiz!")
 
 playing= input("do you want to play? ")
-
+score=0
 if playing != "yes":
     quit()
 
 print("okay! lets play:)")
+
+answer = input("what is my full name? ").lower
+if answer== "rivka finkelstein":
+    print('correct!')
+    score+=1
+else:
+    print("incorrect")
