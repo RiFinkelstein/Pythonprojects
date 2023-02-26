2ed810bf79b0813123a426a7019f27ef0ea64329
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-02-26 19:54:40 +0200

    Update remove_duplicaes_from_list.py

--- remove_duplicaes_from_list.py
+++ remove_duplicaes_from_list.py
@@ -1,8 +1,7 @@
-list_a = [1, 5, 3, 4, 2, 6]
-list_b = [4, 7, 8, 2]
+list_a = [1, 5, 3, 4, 2, 6, 1, 6]
 list_c = []
 
 for i in list_a:
-    if i not in list_b:
+    if i not in list_c:
         list_c.append(i)
 print(list_c)
