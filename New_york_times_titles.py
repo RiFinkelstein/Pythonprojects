bed1d0034bebe4116daac025bbeb517bf007db3e
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-03-20 22:49:55 +0200

    Create New_york_times_titles.py

---
+++ New_york_times_titles.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.nytimes.com/'
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+headlines = soup.find_all('h2')
+
+for headline in headlines:
+    print(headline.text)
