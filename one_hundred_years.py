b671ecd5f3c4d376bd0f77b8976b9e61ef0dc224
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-03-27 23:16:36 -0400

    Update one_hundred_years.py

--- one_hundred_years.py
+++ one_hundred_years.py
@@ -1,8 +1,24 @@
 import datetime
+while True:
+    # Ask the user to enter their name and age
+    name = input("What is your name? ")
+    age = int(input("How old are you? "))
+    
+    # Validate age input
+    try:
+        age = int(age)
+    except ValueError:
+        print("Invalid age input. Please enter a valid integer.")
+        continue
+    
+    # Calculate the year they will turn 100
+    current_year = datetime.datetime.now().year
+    year_when_100 = current_year + (100 - age)
 
-name = input("What is your name? ")
-age = int(input("How old are you? "))
-current_year = datetime.datetime.now().year
-year_when_100 = current_year + (100 - age)
-
-print(f"Hi {name}! You will turn 100 years old in the year {year_when_100}.")
+    print(f"Hi {name}! You will turn 100 years old in the year {year_when_100}.")
+    
+    
+    # Ask if the user wants to continue
+    answer = input("Do you want to continue? (y/n)")
+    if answer.lower() == "n":
+        break
