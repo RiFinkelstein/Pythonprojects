15ae939e98ef6b99e7d1e35e531957aedeed5e25
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-03-30 14:02:03 -0400

    Create user_name_backwords.py

---
+++ user_name_backwords.py
@@ -0,0 +1,6 @@
+#user enters anem and gets name backwords
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+full_name = last_name + " " + first_name
+print("Your name in reverse order is:", full_name)
