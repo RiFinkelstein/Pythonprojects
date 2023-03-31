925ecd961d2bc82b59fa2fbf62cfdf26d1e7f53b
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-03-31 14:08:32 -0400

    Create first_and_last_equal.py

---
+++ first_and_last_equal.py
@@ -0,0 +1,9 @@
+def first_last_equal(numbers):
+    """
+    Check if the first and last number of a given list are the same.
+    If they are the same, return True. Otherwise, return False.
+    """
+    if numbers[0] == numbers[-1]:
+        return True
+    else:
+        return False
