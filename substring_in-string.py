58ca8e25ada922566015c3064a91ba7a36c34833
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-04-01 23:28:39 -0400

    Create substring_in-string.py

---
+++ substring_in-string.py
@@ -0,0 +1,7 @@
+#this is a program that counts the amount of times emma is in the string
+string = "Emma is a great programmer. Emma is also a great singer."
+substring = "Emma"
+
+count = string.count(substring)
+
+print("The substring '{}' appears {} times in the string.".format(substring, count))
