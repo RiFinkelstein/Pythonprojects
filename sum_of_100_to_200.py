603b3aa5e76cd973a780cc6cb85bee4ef1613988
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-04-05 18:01:46 -0400

    Create sum_of_100_to_200.py

---
+++ sum_of_100_to_200.py
@@ -0,0 +1,9 @@
+# Initialize a variable to hold the sum
+total = 0
+
+# Loop through the numbers from 100 to 200 and add them to the total
+for num in range(100, 201):
+    total += num
+
+# Print the total
+print(f"The sum of the numbers from 100 to 200 is {total}")
