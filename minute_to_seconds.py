8ab630b66a747eb044ca0bda6b6c26b4d4ad911b
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-04-15 22:45:51 -0400

    Create minute_to_seconds.py

---
+++ minute_to_seconds.py
@@ -0,0 +1,8 @@
+# Input the minutes
+minutes = int(input("Enter the number of minutes: "))
+
+# Convert minutes to seconds
+seconds = minutes * 60
+
+# Print the result
+print(minutes, "minutes is equal to", seconds, "seconds.")
