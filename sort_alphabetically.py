ca8be6ffc0344848292eaf60cce1b4c23775baa8
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-04-25 22:13:58 +0300

    Create sort_alphabetically.py

---
+++ sort_alphabetically.py
@@ -0,0 +1,6 @@
+def sort_alphabetically(my_list):
+    return sorted(my_list)
+
+ my_list = ['apple', 'banana', 'orange', 'grape']
+sorted_list = sort_alphabetically(my_list)
+print(sorted_list)  # Output: ['apple', 'banana', 'grape', 'orange']
