de8478ada080ecda5d775ec150591a37f3dd4a52
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-05-03 21:39:59 +0300

    Create replace_word.py

---
+++ replace_word.py
@@ -0,0 +1,9 @@
+original_string = "The quick brown fox jumps over the lazy dog"
+substring_to_replace = "brown"
+replacement_substring = "red"
+
+# Replace the substring
+new_string = original_string.replace(substring_to_replace, replacement_substring)
+
+# Output the new string
+print(new_string)
